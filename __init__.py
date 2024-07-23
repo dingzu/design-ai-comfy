928808311ebdee0f315a9d519f7b40a3aa722066
D\\Z <dzwangyihan@gmail.com>
2024-07-24 02:10:37 +0800

    1

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .blackborder import BlackBorder
+
+NODE_CLASS_MAPPINGS = { "BlackBorder": BlackBorder }
+
+NODE_DISPLAY_NAME_MAPPINGS = { "BlackBorder": "黑色边框" }
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ blackborder.py
@@ -0,0 +1,24 @@
+class BlackBorder:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+			
+			
+            },
+        }
+
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
+
+    FUNCTION = "test"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "design-ai/black-border"
+
+    def test(self):
+        return ()
