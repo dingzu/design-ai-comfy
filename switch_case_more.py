49ebedc3a450043eb8167a0ae01b9451258255c1
dingzu <dzwangyihan@gmail.com>
2024-10-12 14:19:23 +0800

    Update switch_case_more.py
    修改支持 1.21

--- switch_case_more.py
+++ switch_case_more.py
@@ -3,16 +3,19 @@ class AnyType(str):
         return True
     def __ne__(self, __value: object) -> bool:
         return False
-  
+
 any = AnyType("*")
 
 class SwitchCaseMore:
+    def __init__(self):
+        pass
+
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "switch_condition": ("STRING", {"default": "", "multiline": False}),
-                **{f"case_{i}": ("STRING", {"default": "", "multiline": False}) for i in range(1, 17)},
+                "switch_condition": ("STRING", {"default": ""}),
+                **{f"case_{i}": ("STRING", {"default": ""}) for i in range(1, 17)},
                 "input_default": (any,),
             },
             "optional": {
@@ -21,9 +24,8 @@ class SwitchCaseMore:
         }
 
     RETURN_TYPES = (any,)
-    RETURN_NAMES = "?"
     FUNCTION = "switch_case"
-    CATEGORY = "✨✨✨design-ai"
+    CATEGORY = "✨✨✨design-ai/switch-case"
 
     def switch_case(self, switch_condition, input_default, **kwargs):
         case_input_dict = {kwargs[f'case_{i}']: kwargs.get(f'input_{i}') for i in range(1, 17)}
